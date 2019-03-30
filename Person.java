c53cff9da0bbec082dc74d306c605d43007a6530
vipulchavan <vvchavan2011.nbnscoe@gmail.com>
2019-03-30 15:04:37 +0530

    person pojo

---
+++ Person.java
@@ -0,0 +1,38 @@
+public class Person {
+    String Name;
+    String Gender;
+    String Father;
+    String Mother;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public String getFather() {
+        return Father;
+    }
+
+    public void setFather(String father) {
+        Father = father;
+    }
+
+    public String getMother() {
+        return Mother;
+    }
+
+    public void setMother(String mother) {
+        Mother = mother;
+    }
+}
